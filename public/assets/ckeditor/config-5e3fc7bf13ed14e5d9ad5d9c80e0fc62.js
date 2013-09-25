ee05159f8b32a81a057a3b7b8322c8150340f87d
Brady Holt <brady.holt@gmail.com>
2013-09-25 14:03:14 -0500

    removing precompiled assets

--- public/assets/ckeditor/config-5e3fc7bf13ed14e5d9ad5d9c80e0fc62.js
+++
@@ -1,5 +0,0 @@
-/*
-Copyright (c) 2003-2011, CKSource - Frederico Knabben. All rights reserved.
-For licensing, see LICENSE.html or http://ckeditor.com/license
-*/
-CKEDITOR.editorConfig=function(e){e.filebrowserBrowseUrl="/ckeditor/attachment_files",e.filebrowserFlashBrowseUrl="/ckeditor/attachment_files",e.filebrowserFlashUploadUrl="/ckeditor/attachment_files",e.filebrowserImageBrowseLinkUrl="/ckeditor/pictures",e.filebrowserImageBrowseUrl="/ckeditor/pictures",e.filebrowserImageUploadUrl="/ckeditor/pictures",e.filebrowserUploadUrl="/ckeditor/attachment_files",e.toolbar_mini=[["Undo","Redo","-","Find","Replace","-","SelectAll","RemoveFormat","Source"],["Bold","Italic","Underline","Strike","-","Subscript","Superscript","Link","Unlink"],["NumberedList","BulletedList","-","Outdent","Indent"],["JustifyLeft","JustifyCenter","JustifyRight","JustifyBlock"],["TextColor","BGColor","Image","Table","HorizontalRule"],["Styles","Format","Font","FontSize","TextColor","BGColor"]]};
