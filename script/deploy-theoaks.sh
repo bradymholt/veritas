11140339ed7fa775b6d7b12002b2231c1e72aab1
Brady Holt <brady.holt@gmail.com>
2014-09-10 22:13:46 -0500

    deploying

--- script/deploy-theoaks.sh
+++ script/deploy-theoaks.sh
@@ -11,17 +11,17 @@ git pull
 echo "rake assets:precompile"
 bundle exec rake assets:precompile RAILS_ENV=production
 echo "copying files..."
-rsync -rvuz --delete ~/dev/veritas/ bholt@hfbctheoaks.com:web/theoaks --exclude='.git/' --exclude='log/' --exclude='tmp/cache'
+rsync -rvuz --delete ~/dev/veritas/ bholt@www.hfbctheoaks.com:web/theoaks --exclude='.git/' --exclude='log/' --exclude='tmp/cache'
 echo "removing local precompiled assets"
 rm -r ~/dev/veritas/public/assets/*
 echo "bundle install"
-ssh bholt@hfbctheoaks.com 'cd ~/web/theoaks && bundle install'
+ssh bholt@www.hfbctheoaks.com 'cd ~/web/theoaks && bundle install'
 echo "rake db:migrate"
-ssh bholt@hfbctheoaks.com 'cd ~/web/theoaks && bundle exec rake db:migrate RAILS_ENV="production"'
+ssh bholt@www.hfbctheoaks.com 'cd ~/web/theoaks && bundle exec rake db:migrate RAILS_ENV="production"'
 echo "rake tmp:clear"
-ssh bholt@hfbctheoaks.com 'cd ~/web/theoaks && bundle exec rake tmp:clear'
+ssh bholt@www.hfbctheoaks.com 'cd ~/web/theoaks && bundle exec rake tmp:clear'
 echo "rake log:clear"
-ssh bholt@hfbctheoaks.com 'cd ~/web/theoaks && bundle exec rake log:clear'
+ssh bholt@www.hfbctheoaks.com 'cd ~/web/theoaks && bundle exec rake log:clear'
 echo "touch tmp/restart.txt"
-ssh bholt@hfbctheoaks.com 'touch ~/web/theoaks/tmp/restart.txt'
+ssh bholt@www.hfbctheoaks.com 'touch ~/web/theoaks/tmp/restart.txt'
 echo "Deploy Successful!"
