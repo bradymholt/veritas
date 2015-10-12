b13cfca60dc1102dfc24db6df9a729dafbd6e226
Brady Holt <brady.holt@gmail.com>
2015-10-12 15:32:19 -0500

    Support multiple (comma delimited) passwords

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,8 @@ class SessionsController < ApplicationController
      session[:role] = 'admin'
      set_role_cookie
      redirect_to session[:login_return_to] || root_path
-    elsif params[:password] == Setting.cached.user_password
+   elsif params[:password] == Setting.cached.user_password
+       || Setting.cached.user_password.split(",").include?(params[:password]) 
      session[:role] = 'user'
      set_role_cookie
      redirect_to session[:login_return_to] || root_path
@@ -16,8 +17,8 @@ class SessionsController < ApplicationController
     flash[:error] = "Invalid Password"
     redirect_to root_path
     end
-    
-   
+
+
   end
 
   def destroy
